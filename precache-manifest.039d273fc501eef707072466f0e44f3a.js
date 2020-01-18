33bdf15fe46787aabd5915af5b306066524e3ab5
Mykola Shwayko <kolik2001@gmail.com>
2020-01-18 22:25:00 +0200

    Updates

---
+++ precache-manifest.039d273fc501eef707072466f0e44f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa10e358c2e281e96461224c277cdaf",
+    "url": "/Pokedex1.0/index.html"
+  },
+  {
+    "revision": "26c1061117d0f31b8c0a",
+    "url": "/Pokedex1.0/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "c74cfa6ed688c0124716",
+    "url": "/Pokedex1.0/static/css/main.f867f56e.chunk.css"
+  },
+  {
+    "revision": "26c1061117d0f31b8c0a",
+    "url": "/Pokedex1.0/static/js/2.6aa4e44b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Pokedex1.0/static/js/2.6aa4e44b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c74cfa6ed688c0124716",
+    "url": "/Pokedex1.0/static/js/main.7965d94a.chunk.js"
+  },
+  {
+    "revision": "abffab60b47a9fda6a52",
+    "url": "/Pokedex1.0/static/js/runtime-main.ad838734.js"
+  }
+]);
